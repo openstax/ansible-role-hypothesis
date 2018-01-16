f2451557b707fe4b6e3668e352c4d7848cf3779e
m1yag1 <mike.arbelaez@rice.edu>
2018-01-16 14:23:46 -0600

    added box-exporter tasks (#14)
    * added box-exporter tasks
    * added annotation export query file
    * added the crontab task for research export
    * fix virtualenv var
    * added crontab for annotation export
    * removed exec command from init_script template
    * renamed the annotations_export.sql file to annotation_export.sql and updated template task

---
+++ files/annotation_export.sql
@@ -0,0 +1,13 @@
+SELECT
+  id AS annotation_id
+, created AS created_on
+, updated AS updated_on
+, CASE WHEN deleted = TRUE THEN 'TRUE' ELSE 'FALSE' END as is_deleted
+, target_selectors -> 0 ->> 'research_identifier' as research_id
+, CASE WHEN LENGTH(text) > 1000
+    THEN CONCAT(SUBSTRING(text for 1000), '[TRUNCATED]')
+    ELSE text
+  END as annotated_text
+, target_selectors -> 0 ->> 'content' AS highlighted_text
+, target_uri AS module_id
+FROM annotation;
